8454a8d7d8a5a2481cb003a7a954347d1d345d66
Arkadiusz Nikołajew <anikolajew@gmail.com>
2021-05-24 18:18:05 +0200

    Add chat consumer

--- config/asgi.py
+++ config/asgi.py
@@ -2,10 +2,17 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from appchat.chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appchat.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application()
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )
 })
